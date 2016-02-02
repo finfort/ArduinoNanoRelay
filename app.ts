8e263bf3b8ddf5c33b9ea1da74eef4788be95e1c
finfort <anatoliy.ruchka@gmail.com>
2016-02-02 18:06:39 +0300

    config compile for TS

--- app.ts
+++ app.ts
@@ -1,13 +1,64 @@
+// server.js
+var express = require('express');
+var app = express();
+var httpServer = require("http").createServer(app);
 import * as five from 'johnny-five';
+
+var io = require('socket.io')(httpServer);
+
+var port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+httpServer.listen(port);
+console.log('Server available at http://localhost:' + port);
+let relay;
+//Arduino board connection
+ 
 const board = new five.Board({ port: "COM3" });
-
 board.on("ready", function() {
+    console.log('Arduino connected');
+    relay = new five.Relay(10);
+});
+ 
+//Socket connection handler
+io.on('connection', function(socket) {
+    console.log(socket.id);
+    socket.on('relay:on', function(data) {
+        relay.on();
+        console.log('relay ON RECEIVED');
+    });
 
-    let relay = new five.Relay(10);
-    relay.toggle();
-
-    this.repl.inject({
-        relay: relay
+    socket.on('relay:off', function(data) {
+        relay.off();
+        console.log('relay OFF RECEIVED');
 
     });
 });
+
+console.log('Waiting for connection');
+ 
+ 
+
+// const board = new five.Board({ port: "COM3" });
+
+// board.on("ready", function() {
+
+//     let relay = new five.Relay(10);
+//     this.repl.inject({
+//         relay: relay
+//     });
+//     if (relay.isOn) {
+//         console.log('turn off');
+//         relay.off();
+//     } else {
+//         console.log('turn on');
+//         relay.on();
+//     }
+//     console.log("Button pressed");
+    
+// });
