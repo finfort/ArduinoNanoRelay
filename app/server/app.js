db4500746d77094856d0ca8e5e58a5d34e0fe867
Anatolii Ruchka <anatoliy.ruchka@gmail.com>
2016-02-07 18:32:41 +0300

    get work with scketch

--- app/server/app.js
+++ app/server/app.js
@@ -1,27 +1,50 @@
 //  tsc --sourcemap --outDir .\app\server\ .\app\server\app.ts
+//  bower install angularjs#1.4.9 socket.io-client angular-socket-io  --save
 var express = require('express');
+
+// import * as express from "express";
 var app = express();
 var httpServer = require("http").createServer(app);
 var five = require('johnny-five');
+
+// import * as five from "johnny-five";
 var EtherPort = require("etherport");
+
+// import * as Etherport from "etherport";
 var io = require('socket.io')(httpServer);
-var port = 3000;
+
+var Firmata = require("firmata").Board;
+
+var port = 8000;
 app.use(express.static(__dirname + '/client/public'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/client/public/index.html');
 });
+
 httpServer.listen(port);
 console.log('Server available at http://localhost:' + port);
+
 var relay;
+
 //Arduino board connection
 var board = new five.Board({
     port: new EtherPort(3030)
 });
+
+// var board = new Firmata(new EtherPort(3030));
 // var board = new five.Board({ port: "COM3" });
 board.on("ready", function () {
     console.log('Arduino connected');
     relay = new five.Relay(6);
+    // console.log("Arduino connected using Firmata.js!");
+    // var state = 1;
+    // this.pinMode(6, this.MODES.OUTPUT);
+    // setInterval(function() {
+    //     this.digitalWrite(6, (state ^= 1));
+    // }.bind(this), 500);
 });
+
 //Socket connection handler
 io.on('connection', function (socket) {
     console.log(socket.id);
@@ -29,9 +52,11 @@ io.on('connection', function (socket) {
         relay.on();
         console.log('relay ON RECEIVED');
     });
+
     socket.on('relay:off', function (data) {
         relay.off();
         console.log('relay OFF RECEIVED');
     });
 });
+
 console.log('Waiting for connection');
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
 
 /// <reference path="johnny-five/johnny-five.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="express/express.d.ts" />
