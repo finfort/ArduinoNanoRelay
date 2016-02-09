4f2351235ddddba1c856ecbf0540280309655647
finfort <anatoliy.ruchka@gmail.com>
2016-02-09 12:16:52 +0300

    git heroku

--- app/server/app.js
+++ app/server/app.js
@@ -2,10 +2,11 @@
 var express = require("express");
 var app = express();
 var httpServer = require("http").createServer(app);
+var five = require("johnny-five");
 var EtherPort = require("etherport");
 var io = require('socket.io')(httpServer);
 var favicon = require('serve-favicon');
-var Firmata = require("firmata").Board;
+// var Firmata = require("firmata").Board;
 app.set('port', (process.env.PORT || 8000));
 // var port = process.env.PORT || 8000;
 app.use(express.static(__dirname + '/client/public'));
@@ -18,35 +19,39 @@ app.get('/', function (req, res) {
 // var hostname = process.env.HOSTNAME || 'localhost';
 httpServer.listen(app.get('port'));
 console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
-var relay;
 //Arduino board connection
-// var board = new five.Board({ 
-//   port: new EtherPort(3030) 
-// });
-var board = new Firmata(new EtherPort(3030));
-// var board = new five.Board({ port: "COM3" });
-board.on("ready", function () {
-    // console.log('Arduino connected');
-    // relay = new five.Relay(6);
-    console.log("Arduino connected using Firmata.js!");
-    var state = 1;
-    this.pinMode(6, this.MODES.OUTPUT);
-    setInterval(function () {
-        this.digitalWrite(6, (state ^= 1));
-    }.bind(this), 500);
+var board = new five.Board({
+    port: new EtherPort(3030)
 });
-//Socket connection handler
+//var board = new Firmata(new EtherPort(3030));
+// var board = new five.Board({ port: "COM3" });
+var relay;
+board.on("ready", function () {
+    console.log('Arduino connected');
+    relay = new five.Relay({
+        pin: 6,
+        type: "NC"
+    });
+    setInterval(function () {
+        relay.toggle();
+    }.bind(this), 500);
+    // console.log("Arduino connected using Firmata.js!");
+    // var state = 1;
+    // this.pinMode(6, this.MODES.OUTPUT);
+    // setInterval(function() {
+    //     this.digitalWrite(6, (state ^= 1));
+    // }.bind(this), 500);
+});
+// Socket connection handler
 // io.on('connection', function(socket) {
 //     console.log("socket id " + socket.id);
 //     socket.on('relay:on', function(data) {
 //         relay.on();
 //         console.log('relay ON RECEIVED');
 //     });
-// 
 //     socket.on('relay:off', function(data) {
 //         relay.off();
 //         console.log('relay OFF RECEIVED');
-// 
 //     });
 // });
 console.log('Waiting for connection');
--- app/server/app.ts
+++ app/server/app.ts
@@ -7,7 +7,7 @@ var EtherPort = require("etherport");
 var io = require('socket.io')(httpServer);
 var favicon = require('serve-favicon');
 
-var Firmata = require("firmata").Board;
+// var Firmata = require("firmata").Board;
 
 app.set('port', (process.env.PORT || 8000));
 // var port = process.env.PORT || 8000;
@@ -23,44 +23,43 @@ app.get('/', function(req, res) {
 // var hostname = process.env.HOSTNAME || 'localhost';
 httpServer.listen(app.get('port'));
 console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
-
-var relay;
 //Arduino board connection
-
-// var board = new five.Board({ 
-//   port: new EtherPort(3030) 
-// });
-
-var board = new Firmata(new EtherPort(3030));
+var board = new five.Board({ 
+  port: new EtherPort(3030) 
+});
+//var board = new Firmata(new EtherPort(3030));
 // var board = new five.Board({ port: "COM3" });
-
+var relay;
 board.on("ready", function() {
-    // console.log('Arduino connected');
-    // relay = new five.Relay(6);
-
-    console.log("Arduino connected using Firmata.js!");
-    var state = 1;
-
-    this.pinMode(6, this.MODES.OUTPUT);
-
+    console.log('Arduino connected');
+    relay = new five.Relay({
+        pin: 6,
+        type: "NC"
+    });
     setInterval(function() {
-        this.digitalWrite(6, (state ^= 1));
+        relay.toggle();
     }.bind(this), 500);
+    // console.log("Arduino connected using Firmata.js!");
+    // var state = 1;
+    // this.pinMode(6, this.MODES.OUTPUT);
+    // setInterval(function() {
+    //     this.digitalWrite(6, (state ^= 1));
+    // }.bind(this), 500);
 });
 
   
-//Socket connection handler
+// Socket connection handler
 // io.on('connection', function(socket) {
 //     console.log("socket id " + socket.id);
 //     socket.on('relay:on', function(data) {
 //         relay.on();
 //         console.log('relay ON RECEIVED');
 //     });
-// 
+
 //     socket.on('relay:off', function(data) {
 //         relay.off();
 //         console.log('relay OFF RECEIVED');
-// 
+
 //     });
 // });
 
