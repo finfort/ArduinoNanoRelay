58f25ed019122eb1e84a4f45997807a2d99ae6a2
finfort <anatoliy.ruchka@gmail.com>
2016-02-11 10:41:11 +0300

    heroku test

--- app/server/app.js
+++ app/server/app.js
@@ -2,11 +2,11 @@
 var express = require("express");
 var app = express();
 var httpServer = require("http").createServer(app);
-// import * as five from "johnny-five";
+var five = require("johnny-five");
 var EtherPort = require("etherport");
 var io = require('socket.io')(httpServer);
 var favicon = require('serve-favicon');
-var Firmata = require("firmata").Board;
+// var Firmata = require("firmata").Board;
 app.set('port', (process.env.PORT || 8000));
 // var port = process.env.PORT || 8000;
 app.use(express.static(__dirname + '/client/public'));
@@ -20,27 +20,27 @@ app.get('/', function (req, res) {
 httpServer.listen(app.get('port'));
 console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 //Arduino board connection
-// var board = new five.Board({ 
-//   port: new EtherPort(3030) 
-// });
-var board = new Firmata(new EtherPort(3030));
+var board = new five.Board({
+    port: new EtherPort(3030)
+});
+// var board = new Firmata(new EtherPort(3030));
 // var board = new five.Board({ port: "COM3" });
 var relay;
 board.on("ready", function () {
-    // console.log('Arduino connected');
-    // relay = new five.Relay({
-    //     pin: 6,
-    //     type: "NC"
-    // });
-    // setInterval(function() {
-    //     relay.toggle();
-    // }.bind(this), 500);
-    console.log("Arduino connected using Firmata.js!");
-    var state = 1;
-    this.pinMode(6, this.MODES.OUTPUT);
+    console.log('Arduino connected');
+    relay = new five.Relay({
+        pin: 6,
+        type: "NC"
+    });
     setInterval(function () {
-        this.digitalWrite(6, (state ^= 1));
+        relay.toggle();
     }.bind(this), 500);
+    // console.log("Arduino connected using Firmata.js!");
+    // var state = 1;
+    // this.pinMode(6, this.MODES.OUTPUT);
+    // setInterval(function() {
+    //     this.digitalWrite(6, (state ^= 1));
+    // }.bind(this), 500);
 });
 // Socket connection handler
 // io.on('connection', function(socket) {
