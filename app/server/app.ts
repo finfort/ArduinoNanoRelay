b67e4244e3080881730cb88072598eae5aa69977
finfort <anatoliy.ruchka@gmail.com>
2016-02-11 16:47:34 +0300

    heroku test

--- app/server/app.ts
+++ app/server/app.ts
@@ -2,7 +2,7 @@
 import * as express from "express";
 var app = express();
 var httpServer = require("https").createServer(app);
-// import * as five from "johnny-five";
+import * as five from "johnny-five";
 var EtherPort = require("etherport");
 var io = require('socket.io')(httpServer);
 var favicon = require('serve-favicon');
@@ -21,44 +21,51 @@ app.get('/', function(req, res) {
 httpServer.listen(app.get('port'));
 console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
 //Arduino board connection
-// var board = new five.Board({
-//     port: new EtherPort(3030)
-// });
-var board = new Firmata(new EtherPort(3030));
+var board = new five.Board({
+    port: new EtherPort(3030)
+});
+// var board = new Firmata(new EtherPort(3030));
 // var board = new five.Board({ port: "COM3" });
 var relay;
 board.on("ready", function() {
-    // console.log('Arduino connected');
-    // relay = new five.Relay({
-    //     pin: 6,
-    //     type: "NC"
-    // });
-    // setInterval(function() {
-    //     relay.toggle();
-    // }.bind(this), 500);
-    console.log("Arduino connected using Firmata.js!");
-    var state = 1;
-    this.pinMode(6, this.MODES.OUTPUT);
+    console.log('Arduino connected');
+    relay = new five.Relay({
+        pin: 6,
+        type: "NC"
+    });
     setInterval(function() {
-        this.digitalWrite(6, (state ^= 1));
-    }.bind(this), 3000);
+        relay.toggle();
+    }.bind(this), 500);
+    // console.log("Arduino connected using Firmata.js!");
+    // var state = 1;
+    // this.pinMode(6, this.MODES.OUTPUT);
+    // setInterval(function() {
+    //     this.digitalWrite(6, (state ^= 1));
+    // }.bind(this), 500);
 });
-
-  
 // // Socket connection handler
-// io.on('connection', function(socket) {
-//     console.log("socket id " + socket.id);
-//     socket.on('relay:on', function(data) {
-//         relay.on();
-//         console.log('relay ON RECEIVED');
-//     });
+io.on('connection', function(socket) {
+    console.log("socket id " + socket.id);
+    socket.on('relay:on', function(data) {
+        relay.on();
+        console.log('relay ON RECEIVED');
+    });
 
-//     socket.on('relay:off', function(data) {
-//         relay.off();
-//         console.log('relay OFF RECEIVED');
+    socket.on('relay:off', function(data) {
+        relay.off();
+        console.log('relay OFF RECEIVED');
 
-//     });
-// });
+    });
+    var id = setInterval(function() {
+        socket.send(JSON.stringify(new Date()), function() { })
+    }, 1000)
+
+
+    socket.on("close", function() {
+        console.log("websocket connection close")
+        clearInterval(id)
+    })
+});
 
 console.log('Waiting for connection');
 // setInterval(function() {
