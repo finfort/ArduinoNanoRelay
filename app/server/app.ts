da07ada1baa2c1c7cb59777f7e2b23e94cc2b300
finfort <anatoliy.ruchka@gmail.com>
2016-02-17 17:29:04 +0300

    aws test with express

--- app/server/app.ts
+++ app/server/app.ts
@@ -7,49 +7,50 @@ var EtherPort = require("etherport");
 var io = require('socket.io')(httpServer);
 var favicon = require('serve-favicon');
 var Firmata = require("firmata").Board;
-// app.set('port', (process.env.PORT || 7000));
+app.set('port', (process.env.PORT || 8000));
 
-// app.use(express.static(__dirname + '/client/public'));
-// app.use(favicon(__dirname + '/client/public/favicon.ico'));
-// app.get('/', function(req, res) {
-//     res.sendFile(__dirname + '/client/public/index.html');
-// });
+app.use(express.static(__dirname + '/client/public'));
+app.use(favicon(__dirname + '/client/public/favicon.ico'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/public/index.html');
+});
+
 
 // httpServer.listen(port);
 // console.log('Server available at http://localhost:' + port);
 // var hostname = process.env.HOSTNAME || 'localhost';
 //*****
-// httpServer.listen(app.get('port'));
-// console.log("Express server listening on port http://localhost:%d in %s mode", app.get('port'), app.settings.env);
+httpServer.listen(app.get('port'));
+console.log("Express server listening on port http://localhost:%d in %s mode", app.get('port'), app.settings.env);
 
 //Arduino board connection
 
-// var board = new five.Board({
-//     port: new EtherPort(3030)
-// });
-var board = new Firmata(new EtherPort(3030));
+var board = new five.Board({
+    port: new EtherPort(3030)
+});
+// var board = new Firmata(new EtherPort(3030));
 // var board = new five.Board({ port: "COM3" });
 var relay;
 // try {
     
 
 board.on("ready", function() {
-    // console.log('Arduino connected');
-    // relay = new five.Relay({
-    //     pin: 6,
-    //     type: "NO"
-    // });
+    console.log('Arduino connected');
+    relay = new five.Relay({
+        pin: 6,
+        type: "NO"
+    });
     
     // setInterval(function() {
     //     relay.toggle();
     // }.bind(this), 500);
     
-    console.log("Arduino connected using Firmata.js!");
-    var state = 1;
-    this.pinMode(6, this.MODES.OUTPUT);
-    setInterval(function() {
-        this.digitalWrite(6, (state ^= 1));
-    }.bind(this), 500);
+    // console.log("Arduino connected using Firmata.js!");
+    // var state = 1;
+    // this.pinMode(6, this.MODES.OUTPUT);
+    // setInterval(function() {
+    //     this.digitalWrite(6, (state ^= 1));
+    // }.bind(this), 500);
 });
 board.on("error", function(msg) {
     console.log("Board Error:\n ", msg);
@@ -59,31 +60,31 @@ board.on("error", function(msg) {
 //     console.log("Catch error \n" + error);
 // }
 // // Socket connection handler
-// io.on('connection', function(socket) {
-//     console.log("socket id " + socket.id);
-//     socket.on('relay:on', function(data) {
-//         relay.on();
-//         console.log('relay ON RECEIVED');
-//     });
+io.on('connection', function(socket) {
+    console.log("socket id " + socket.id);
+    socket.on('relay:on', function(data) {
+        relay.on();
+        console.log('relay ON RECEIVED');
+    });
 
-//     socket.on('relay:off', function(data) {
-//         relay.off();
-//         console.log('relay OFF RECEIVED');
+    socket.on('relay:off', function(data) {
+        relay.off();
+        console.log('relay OFF RECEIVED');
 
-//     });
-//     var id = setInterval(function() {
-//         let message =JSON.stringify(new Date());
-//         console.log(message);
-//         socket.emit('foo', message);
-//         // socket.emit(JSON.stringify(new Date()), function() { })
-//     }, 3000)
+    });
+    var id = setInterval(function() {
+        let message =JSON.stringify(new Date());
+        console.log(message);
+        socket.emit('foo', message);
+        // socket.emit(JSON.stringify(new Date()), function() { })
+    }, 3000)
 
 
-//     socket.on("disconnect", function() {
-//         console.log("websocket connection close")
-//         clearInterval(id)
-//     })
-// });
+    socket.on("disconnect", function() {
+        console.log("websocket connection close")
+        clearInterval(id)
+    })
+});
 
 console.log('Waiting for connection');
 // setInterval(function() {
